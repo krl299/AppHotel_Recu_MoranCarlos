d1fdd8e31d4dbf868aff9ec397691a81e1d9f608
marquez07 <francisco20007@hotmail.es>
2019-11-12 09:27:53 +0100

    Habilitado y deshabilitado los campos de reserva del salon

--- src/di_t2_apphotel/SalonHabanaController.java
+++ src/di_t2_apphotel/SalonHabanaController.java
@@ -1,17 +1,28 @@
 package di_t2_apphotel;
 
 import entidades.Cliente;
+import entidades.ReservasalonPK;
+import entidades.Reservasalon;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
@@ -22,8 +33,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -38,6 +54,8 @@ public class SalonHabanaController implements Initializable {
 
     private EntityManager em;
     private EntityManagerFactory emf;
+    private boolean errorFormato = false;
+    private ArrayList<String> lista = new ArrayList<String>();
 
     @FXML
     private TextField textFieldDNI;
@@ -81,6 +99,10 @@ public class SalonHabanaController implements Initializable {
     private Button btnCancelar;
     @FXML
     private Button btnAceptar;
+    @FXML
+    private VBox grupo_rb;
+    @FXML
+    private Label labelPersona;
 
     /**
      * Initializes the controller class.
@@ -88,28 +110,97 @@ public class SalonHabanaController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        lista.add("No precisa");
+        lista.add("Buffet vegetariano");
+        lista.add("Buffet NO vegetariano");
+        lista.add("Carta");
+
+        comboBoxTipoCocina.setValue(lista.get(0));
+        comboBoxTipoCocina.setItems(FXCollections.observableList(lista));
+
     }
 
     @FXML
     private void onActionBanquete(ActionEvent event) {
+        //Habilitamos y desabilitamos lo que necesitamos
+        deshabilitar();
+        labelPersona.setDisable(false);
+        textFiedlPersonas.setDisable(false);
+        labelTipoCocina.setDisable(false);
+        comboBoxTipoCocina.setDisable(false);
+        datePickerFecha.setDisable(false);
+        labelTipoElegido.setDisable(false);
+
+        //Cambiamos el label del tipo de evento
+        labelTipoElegido.setText("Tipo Elegido: Banquete");
+        labelTipoElegido.setStyle("-fx-text-fill: #37b3e4");
+        
+        ReservasalonPK reserva = new ReservasalonPK();
+
+        if (textFiedlPersonas.getText() != "") {
+            textFiedlPersonas.getText();
+        } else {
+            errorFormato = true;
+        }
+
+        comboBoxTipoCocina.getItems();
+
+        if (reserva.getFecha() != null) {
+            reserva.getFecha();
+        } else {
+            errorFormato = true;
+        }
+
     }
 
     @FXML
     private void onActionJornada(ActionEvent event) {
+        //Habilitamos y desabilitamos lo que necesitamos
+        deshabilitar();
+        labelPersona.setDisable(false);
+        textFiedlPersonas.setDisable(false);
+        datePickerFecha.setDisable(false);
+        labelTipoElegido.setDisable(false);
+        
+        labelTipoElegido.setStyle("-fx-text-fill: #ff5c5c");
+        //Cambiamos el label del tipo de evento
+        labelTipoElegido.setText("Tipo Elegido: Jornada");
     }
 
     @FXML
     private void onActionCongreso(ActionEvent event) {
+        //Habilitamos y desabilitamos lo que necesitamos
+        deshabilitar();
+        labelPersona.setDisable(false);
+        textFiedlPersonas.setDisable(false);
+        labelCuantas.setDisable(false);
+        checkBoxHabitaciones.setDisable(false);
+        textFieldHab.setDisable(false);
+        datePickerFecha.setDisable(false);
+        labelTipoElegido.setDisable(false);
+        labelNumDias.setDisable(false);
+        textFieldDIas.setDisable(false);
+
+        //Cambiamos el label del tipo de evento
+        labelTipoElegido.setText("Tipo Elegido: Congreso");
+        labelTipoElegido.setStyle("-fx-text-fill: #e6ba37");
     }
 
     @FXML
     private void onActionNecesitasHab(ActionEvent event) {
+        if (checkBoxHabitaciones.isSelected()) {
+            textFieldHab.setDisable(false);
+            labelCuantas.setDisable(false);
+        } else {
+            textFieldHab.setDisable(true);
+            labelCuantas.setDisable(true);
+        }
     }
 
     @FXML
     private void onActionBtnBuscar(KeyEvent event) {
         if (event.getCode().equals(KeyCode.ENTER)) {
-            if ((!textFieldDNI.equals("")) && (textFieldNombre.getText() != null)) {
+            if ((!textFieldDNI.equals("")) && (textFieldDNI.getText() != null)) {
                 iniciarConexion();
 
                 Query queryCliente = em.createQuery("select c from Cliente c where c.dni='" + textFieldDNI.getText() + "'");
@@ -121,8 +212,8 @@ public class SalonHabanaController implements Initializable {
                     textFieldDireccion.setText(cliente.getDireccion());
                     textFieldTelefono.setText(cliente.getTelefono());
                 }
-                pararConexion();
             }
+            grupo_rb.setDisable(false);
         }
     }
 
@@ -160,10 +251,28 @@ public class SalonHabanaController implements Initializable {
 
     @FXML
     private void onActionCancelar(ActionEvent event) {
+
     }
 
     @FXML
     private void onActionAceptar(ActionEvent event) {
     }
 
+    public DatePicker getfecha() {
+        return datePickerFecha;
+    }
+
+    public void deshabilitar() {
+        labelPersona.setDisable(true);
+        textFiedlPersonas.setDisable(true);
+        comboBoxTipoCocina.setDisable(true);
+        checkBoxHabitaciones.setDisable(true);
+        textFieldDIas.setDisable(true);
+        labelNumDias.setDisable(true);
+        labelCuantas.setDisable(true);
+        datePickerFecha.setDisable(true);
+        labelTipoElegido.setDisable(true);
+        textFieldHab.setDisable(true);
+        labelTipoCocina.setDisable(true);
+    }
 }
